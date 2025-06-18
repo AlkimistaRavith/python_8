2fa614176e9de4d17de270209715cf8e98e81356
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-18 02:27:00 +0000

    avance

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 from personaje import Personaje
+import random
+
+print("¡Bienvenido a Gran Fantasía!")
+nombre = input("Por favor indique el nombre de su personaje: ")
+
+
+personaje = Personaje(nombre)
+
+print(personaje.estado)
+
+orco = Personaje("Orco")
+prob = personaje.probabilidad(orco)
+opcion = Personaje.mostrar_dialogo(prob)
+
+while opcion == 1:
+    resultado = "win" if random.uniform(0,100) < prob else "lose"
+    if resultado == "win":
+        print("""
+¡Le has ganado al orco, felicidades!
+¡Recibirás 50 puntos de experiencia!
+""")
+        personaje.estado = 50
+        orco.estado = -30
+    else:
+        print("""
+¡Oh no! ¡El orco te ha ganado!
+¡Has perdido 30 puntos de experiencia!
+""")
+        personaje.estado = -30
+        orco.estado = 50
+    
+    print(personaje.estado)
+    print(orco.estado)
+
+    prob = personaje.probabilidad(orco)
+    opcion = Personaje.mostrar_dialogo(prob)
+
+print("has huido con éxito.")
--- personaje.py
+++ personaje.py
@@ -14,6 +14,7 @@ NOMBRE: {self.nombre}
 NIVEL: {self.nivel}
 EXPERIENCIA: {self.experiencia}"""
 
+    #Para modificar el estado
     @estado.setter
     def estado(self, exp):
         tmp_exp = self.experiencia + exp
@@ -45,17 +46,27 @@ EXPERIENCIA: {self.experiencia}"""
         #sobrecarga para metodo de igual
         return self.nivel == oponente.nivel
 
+    def probabilidad(self, oponente):
+        #Si el jugador es menor al orco, tiene un 33% de probabilidades de ganar.
+        if self < oponente:
+            return 33
+        #Si el jugador es mayor al orco, tiene un 66% de probabilidades de ganar.
+        elif self > oponente:
+            return 66
+        #Si el jugador es igual al orco, tiene un 50% de probabilidades de ganar.
+        else:
+            return 50
+        
+    @staticmethod   
+    def mostrar_dialogo(prob):
+        return int(input(f"""
+¡Oh no!, ¡Ha aparecido un Orco!
+Con tu nivel actual, tienes {prob}% de probabilidades de ganarle al Orco.
+Si ganas, ganarás 50 puntos de experiencia y el orco perderá 30.
+Si pierdes, perderás 30 puntos de experiencia y el orco ganará 50.
+¿Qué deseas hacer?
+1. Atacar
+2. Huir
+Ingresa tu opción: """))
 
 
-
-    #retornando variable como texto    
-    def __str__(self):
-        return f"{self.nombre}"
-
-
-
-orco = Personaje("orco")
-jugador = Personaje("guts")
-
-jugador.estado = 120
-print(jugador.estado)
